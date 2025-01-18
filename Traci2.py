7b16f807df67f8673813054c174ff4a2728d5b91
ahmad1441 <142044527+RoadwayVR@users.noreply.github.com>
2025-01-18 14:56:28 -0500

    Update Traci2.py

--- Traci2.py
+++ Traci2.py
@@ -1,39 +1,40 @@
-#Step 1: Add modules to provide access to specific libraries and functions 
-import os  # Provides functions to handle file paths, directories, environment variables
-import sys  # Provides access to Python-specific system parameters and functions
-
-#Step 2: establish path to SUMO (SUMO_HOME)
+# Step 1: Add modules to provide access to specific libraries and functions
+import os # Module provides functions to handle file paths, directories, environment variables
+import sys # Module provides access to Python-specific system parameters and functions
+# Step 2: Establish path to SUMO (SUMO_HOME)
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
 else:
-    sys.exit("Please declare the environment variable 'SUMO_HOME'")
+    sys.exit("Please declare environment variable 'SUMO_HOME'")
+# Step 3: Add Traci module to provide access to specific libraries and functions
+import traci # Module for controlling SUMO simulations via TraCI
+# Step 4: Define SUMO configuration
+Sumo_config = [
+    'sumo-gui',
+    '-c', 'Traci.sumocfg',
+    '--step-length', '0.05',
+    '--delay', '1000',
+    '--lateral-resolution', '0.1'
+]
+# Step 5: Open connection between SUMO and Traci
+traci.start(Sumo_config)
+# Step 6: Define Variables
+vehicle_speed = 0
+total_speed = 0
+# Step 7: Define Functions
+def process_vehicles():
+    #vehicles_list = traci.vehicle.getIDList()
+    if 'veh1' in traci.vehicle.getIDList():
+        traci.vehicle.moveToXY('veh1', 'E0.62', '0', 13, -10, 0, keepRoute=2)
+        position = traci.vehicle.getPosition('veh1') # Get the (x, y) position of the vehicle
+        angle = traci.vehicle.getAngle('veh1')
+        print(f"Vehicle ID: {'veh1'}, Position: {position}, Angle: {angle}")
+# Step 8: Take simulation steps until there are no more vehicles in the network
+while traci.simulation.getMinExpectedNumber() > 0:
+    traci.simulationStep() # Move simulation forward 1 step
+    # Here you can decide what to do with simulation data at each step
+    process_vehicles()
+# Step 9: Close connection between SUMO and Traci
+traci.close()
 
-#Step 3: Add Traci module to provide access to specific libraries and functions 
-import sumolib  
-
-#Step 4: Define Sumo configuration
-#IT DOES NOT HAVE
-
-#Step 5:  Open connection between SUMO and SUMOLib
-net = sumolib.net.readNet('Traci.net.xml')  # Load network file for analysis
-
-#Step 6: Define Variables
-speedsum = 0
-edgecount = 0
-avgSpeed = 0
-
-#Step 7: Define Functions 
-for edge in net.getEdges():
-    speedsum += edge.getSpeed()  # Sum up the speeds of all edges
-    edgecount += 1  # Count the number of edges
-
-if edgecount > 0:
-    avgSpeed = speedsum / edgecount  # Calculate average speed
-    print(f"Average speed in all edges: {avgSpeed}")
-
-#Step 8: Take simulation steps until there are no more vehicles in the network
-#IT DOES NOT HAVE
-
-#Step 9: close connection between sumo and traci
-#IT DOES NOT HAVE
