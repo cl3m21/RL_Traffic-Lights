742ae3942568eca3fbb746a4ec99980352effa3e
Aurel <clement.aurel@capgemini.com>
2025-11-06 12:39:11 +0000

    SKLearn

--- QOS/TrainTestSplit/SKLearn.py
+++ QOS/TrainTestSplit/SKLearn.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 #importing dataset
 df = pd.read_csv('qws1.csv', encoding = 'latin-1')
@@ -20,31 +20,49 @@ print("Training set shape : ", X_train.shape, y_train.shape)
 
 print("Test set shape : " , X_test.shape, y_test.shape)
 
-#Linear Regression model
-reg = LinearRegression(fit_intercept = True)
+### Linear Regression model###
+##
+#
+
+regLin = LinearRegression(fit_intercept = True)
 
 #Train the model on training set X_train & y_train
 
-
-reg.fit(X_train, y_train)
+regLin.fit(X_train, y_train)
 
 # predict on test set with model based on feature , target being the classification value
 
-y_pred = reg.predict(X_test)
-print("prediction values from model output")
-print(y_pred)
+y_predLin = regLin.predict(X_test)
+print("prediction values from model output Linear Regression")
+print(y_predLin)
 
 #then compare with test set results from dataset and measure performance
 #put output from test Y prediction into np array to compare them
 
-np.array(y_test) == np.array(y_test)
+np.array(y_test) == np.array(y_predLin)
 
 # all prediction classifications matches the values from the y_test value confirming our model works perfectly with Linear Regression
 #Measuring performance on test set
 
 print("test training over the test set")
-score = reg.score(X_test, y_test)
+score = regLin.score(X_test, y_test)
 print(score)
+
+#
+##
+### End Linear Regression Model
+
+### Start Logitics Regression Model
+##
+#
+
+regLog = LogisticRegression(fit_intercept = True)
+
+
+
+### End Logistic Regression Model
+
+
 
 #pipeline
 
